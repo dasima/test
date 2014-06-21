8d77a257951e8b0c3f33bea5711e63a022254b53
haoya zha <haoya@ubuntu.(none)>
2014-06-21 10:05:12 +0800

    commit first time

---
+++ a.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(int argc ,char* argv[])
+{
+     printf("this is another!\n");   
+     return 0;
+}
---
+++ test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(int argc , char* argv[])
+{
+    int i ;
+    for(i = 0; i < 3; i++)
+        printf("hahaha\n");
+    printf("hello world!\n");
+    return 0;
+}
